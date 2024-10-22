324f78721a2a0a960f09d55a75e3f45640537f0e
Federico-sorsaburu <Sorsaburu.federico98@gmail.com>
2024-10-21 21:00:20 -0300

    2da entrega

--- js/index.js
+++ js/index.js
@@ -32,3 +32,65 @@ do {
 if (intentos === maxIntentosPermitidos && password !== 'clave123') {
     alert("Has alcanzado el número máximo de intentos.");
 }
+
+
+
+// total del carrito compras
+
+const carrito__TOTAL = [1000, 400, 1200, 600, 900]
+// console.log(carrito__TOTAL)
+
+
+const TOTAL_A_PAGAR = carrito__TOTAL.reduce((acum, num) => acum + num,0)
+console.log("El total a pagar de tu compra " + TOTAL_A_PAGAR)
+
+
+const listaCOMIDA = ["queso lasal", "Cangrejo Jumbo","Jugo kotail","Filete capo","Ensalada de frutas", "Pizza de queso", "carne kofta","Pastelitos españoles", "tostadas de queso","SHAWARMA DE POLLO"]
+listaCOMIDA.sort((a,b) => b.localeCompare(a));
+console.log(listaCOMIDA)
+
+
+
+let productosComercio = [
+    {
+        nombre: "QUESO LASAL",
+        precio: 18.29
+    },
+    {
+        nombre: "CANGREJO JUMBO",
+        precio: 24.59
+    },
+    {
+        nombre: "JUGO KOKTAIL",
+        precio: 12.99
+    },
+    {
+        nombre: "FILETE CAPO",
+        precio: 60.55
+    },
+    {
+        nombre: "ENSALADA DE FRUTAS ORGÁNICA",
+        precio: 8.55
+    },
+    {
+        nombre: "PIZZA DE QUESO",
+        precio: 18.25
+    },
+    {
+        nombre: "PIZZA MEGA DE QUESO",
+        precio: 21.77
+    },
+    {
+        nombre: "PASTELITOS ESPAÑOLES",
+        precio: 14.99
+    },
+    {
+        nombre: "CARNE KOFTA",
+        precio: 41.55
+    },
+]
+let preciosComercio = productosComercio.map (precios => Math.floor(precios.precio) );
+console.log("Precios actualizados 2024: " + preciosComercio)
+
+let sumaTotal = productosComercio.reduce((acum,precios) => acum + Math.floor(precios.precio), 0 )
+console.log ("total acumulado: " + sumaTotal)
